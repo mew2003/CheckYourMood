80eba363e362d772ab546a37f8821c3568d37e36
mew2003 <mewen.derruau@gmail.com>
2022-11-08 09:48:31 +0100

    header component

---
+++ codeCYM/JS/header-component.js
@@ -0,0 +1,15 @@
+class headerComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.innerHTML = `
+        <link rel="stylesheet" href="../CSS/Register.css">
+        <div class="d-flex mb-4 header-bar align-items-center">
+            <div class="me-auto p-2 LOGO">Check your mood</div>
+            <div class="p-2 header-hover">Stats</div>
+            <div class="p-2 header-hover">Humeurs</div>
+            <div class="p-2 header-hover">H</div>
+        </div>`
+    }
+}
+
+customElements.define('header-component', headerComponent)
