b44ea50c1277f6a410efce4a1ce8c9f0080da1eb
mew2003 <mewen.derruau@gmail.com>
2022-11-19 16:16:15 +0100

    page humeurs complètement fonctionnel (rajouté la description des humeurs facultative ...)

--- Uwamp_2020/www/codeCYM/pages/Humeurs.php
+++ Uwamp_2020/www/codeCYM/pages/Humeurs.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../CSS/humeurs.css">
     <link rel="stylesheet" href="../third-party/fontawesome-free-6.2.0-web/css/all.css">
     <script src="../JS/header-component.js" defer></script>
+    <script src="../JS/humeurs.js"></script>
 </head>
 <body>
     <?php 
@@ -28,31 +29,36 @@
         PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ,
         PDO::ATTR_EMULATE_PREPARES=>false];
 
-    try {
-        // Connexion à la BDD
-        $pdo=new PDO($dsn,$user,$pass,$options);
-        // à modifier : CODE_User, Humeur_Description
-        if (isset($_POST["humeurs-liste"])) {
-            $libele = htmlspecialchars($_POST['humeurs-liste']);
-            $smiley = "";
-            $stmt = $pdo->prepare("INSERT INTO `humeur`(`CODE_User`, `Humeur_Libelle`, `Humeur_Emoji`, `Humeur_Time`, `Humeur_Description`) 
-                                      VALUES (1,:libele,:smiley,CURRENT_TIMESTAMP,'')");
-            $stmt->bindParam("libele", $libele);
-			$stmt->bindParam("smiley", $smiley);
-            $stmt->execute();
-        }
-    } catch (PDOException $e) {
-        // Toute exception est renvoyé ici
-        echo "<h1>Erreur BD ".$e->getMessage();
-    }
 
-    try{
+    try {
         $nomficTypes="./humeurs.csv" ;
         if ( !file_exists($nomficTypes) ) {
             throw new Exception('Fichier '.$nomficTypes.' non trouvé.');
         }
         $listeHumeurs = file($nomficTypes, FILE_IGNORE_NEW_LINES);
     } catch ( Exception $e ) {}
+
+    try {
+        // Connexion à la BDD
+        $pdo=new PDO($dsn,$user,$pass,$options);
+        if (isset($_POST["humeur"]) && $_POST["humeur"] != "") {
+            foreach ($listeHumeurs as $humeur) {
+                if ($humeur == $_POST["humeur"]) {
+                    $libele = htmlspecialchars($_POST['humeur']);
+                    $smiley = $_POST["smiley"];
+                    // /!\ NE PAS OUBLIER DE CHANGER CODE_USER PAR LE CODE SESSION / AJOUTER LA DESCRIPTION !!!!
+                    $requete = $pdo->prepare("INSERT INTO `humeur`(`CODE_User`, `Humeur_Libelle`, `Humeur_Emoji`, `Humeur_Time`, `Humeur_Description`) 
+                                              VALUES (1,:libele,:smiley,CURRENT_TIMESTAMP,'')");
+                    $requete->bindParam("libele", $libele);
+                    $requete->bindParam("smiley", $smiley);
+                    $requete->execute();
+                }
+            }
+        }
+    } catch (PDOException $e) {
+        // Toute exception est renvoyé ici
+        echo "<h1>Erreur BD ".$e->getMessage();
+    }
     ?>
     <header-component></header-component>
     <div class="container">
@@ -62,7 +68,7 @@
         <form class="humeurs-container" action="#" method="post">
             <div class="row border-form">
                 <div class="col col-md-8 col-9">
-                    <input class="humeurs-liste" list="humeurs-liste" name="humeurs-liste" onchange="getSmiley(this)">
+                    <input class="humeurs-liste" list="humeurs-liste" name="humeur" onchange="getSmiley(this)">
                     <datalist id="humeurs-liste" >
                         <?php
                             foreach ($listeHumeurs as $i) {
@@ -72,7 +78,7 @@
                     </datalist>
                 </div>
                 <div class="col col-md-2 col-3 smiley-zone">
-                    <p name="smiley" id="smiley"></p>
+                    <input name="smiley" id="smiley" readonly>
                 </div>
                 <div class="col col-md-2 col-12 envoyer-zone">
                     <button class="bouton-envoyer"><i class="fa-solid fa-location-arrow"></i></button>
@@ -81,48 +87,4 @@
         </form>
     </div>
 </body>
-<script>
-    function refreshTime() {
-        const timeDisplay = document.getElementById("time");
-        const dateString = new Date().toLocaleTimeString();
-        timeDisplay.textContent = dateString;
-    }
-    setInterval(refreshTime, 1000);
-
-    function getSmiley(element) {
-        var saisie = (element.value || element.options[element.selectedIndex].value); 
-        switch ((""+saisie).toLowerCase()) {
-            case 'admiration': smiley = "😊"; break;
-            case 'adoration': smiley = "🤤"; break;
-            case 'appréciation esthétique': smiley = "🖼️"; break;
-            case 'amusement': smiley = "🥳"; break;
-            case 'colère': smiley = "😠"; break;
-            case 'anxiété': smiley = "😰"; break;
-            case 'emerveillement': smiley = "🤩"; break;
-            case 'malaise (embarrassement)': smiley = "😖"; break;
-            case 'ennui': smiley = "🥱"; break;
-            case 'calme (sérénité)': smiley = "😐"; break;
-            case 'confusion': smiley = "😕"; break;
-            case 'envie (craving)': smiley = "🥵"; break;
-            case 'dégoût': smiley = "🤢"; break;
-            case 'douleur empathique': smiley = "💔"; break;
-            case 'intérêt étonné, intrigué': smiley = "🤔"; break;
-            case 'excitation (montée d’adrénaline)': smiley = "🤪"; break;
-            case 'peur': smiley = "😨"; break;
-            case 'horreur': smiley = "😱"; break;
-            case 'intérêt': smiley = "🧐"; break;
-            case 'joie': smiley = "😄"; break;
-            case 'nostalgie': smiley = "🎆"; break;
-            case 'soulagement': smiley = "😌"; break;
-            case 'romance': smiley = "🌹"; break;
-            case 'tristesse': smiley = "😥"; break;
-            case 'satisfaction': smiley = "👍"; break;
-            case 'désir sexuel': smiley = "😏"; break;
-            case 'surprise': smiley = "🙀"; break;
-            default:
-                smiley = "🚫";
-        }
-        document.getElementById('smiley').innerText = smiley;
-    }
-</script>
 </html>
