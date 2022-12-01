4dbc549647e851a678283bc3b9551f8c3b029ca8
mew2003 <mewen.derruau@gmail.com>
2022-12-01 10:38:03 +0100

    MVC ajout de la page stats

--- codeCYM/yasmf/datasource.php
+++ codeCYM/yasmf/datasource.php
@@ -45,7 +45,7 @@ class DataSource
         $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->db;charset=$this->charset";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_PERSISTENT => true
         ];
