d78509a6952286b7b96f41265cc6fd2f5616aaf6
ReDhix <Sebastien.dasilvaoliveira@gmail.com>
2022-12-13 15:31:38 +0100

    vérifications terminées sur la page profil
    si l'utilisateur tape une email fausse ou déjà existante il est averti
    si l'utilisateur tape un pseudo déjà existant il est averti
    si l'utilisateur met une date de naissance supérieur à la date d'aujourd'hui il est averti

--- codeCYM/controllers/accountscontroller.php
+++ codeCYM/controllers/accountscontroller.php
@@ -20,81 +20,89 @@ class AccountsController {
         $resultats = $this->accountsService->getProfile($pdo);
         $view->setVar('resultats',$resultats);
         while($row = $resultats->fetch()) {
-            $view->setVar('email', $row->User_Email);
+            $view->setVar('mail', $row->User_Email);
             $view->setVar('username', $row->User_Name);
             $view->setVar('password', $row->User_Password);
-            $view->setVar('birthDate', $row->User_BirthDate);
+            $view->setVar('dateOfBirth', $row->User_BirthDate);
             $view->setVar('gender', $row->User_Gender);
         }
         return $view;
     }
 
     public function editProfile($pdo) {
+        // lancement de la session
         session_start();
-        $verif = $this->accountsService->getProfile($pdo);
-        $envoyer = HttpHelper::getParam("envoyer");
-        while($row = $verif->fetch()) {
-            $verifEmail = $row->User_Email;
-            $verifUsername = $row->User_Name;
-            $verifDateOfBirth = $row->User_BirthDate;
-            $verifGender = $row->User_Gender;
-        }
+        // création de la vue pour modifié son profil
         $view = new View("CheckYourMood/codeCYM/views/editprofile");
+        // récupération de toutes les données du formulaire
         $email = HttpHelper::getParam("email");
-        $username = HttpHelper::getParam("pseudo");
+        $username = HttpHelper::getParam("username");
         $dateOfBirth = HttpHelper::getParam("dateOfBirth");
         $gender = HttpHelper::getParam("genderList");
         $update = HttpHelper::getParam("envoyer");
+        // récupération du profil de l'utilisateur courant
+        $verif = $this->accountsService->getProfile($pdo);
+        while($row = $verif->fetch()) {
+            $defaultEmail = $row->User_Email;
+            $defaultUsername = $row->User_Name;
+            $defaultDateOfBirth = $row->User_BirthDate;
+            $defaultGender = $row->User_Gender;
+        }
+        // initialisation des variables
         $view->setVar('message', null);
-        $view->setVar('mailChanged', false);
-        $view->setVar('usernameChanged', false);
-        $view->setVar('birthDateChanged', false);
-        $view->setVar('genderChanged', false);
-        $view->setVar('envoyer', $envoyer);
+        $view->setVar('defaultEmail', $defaultEmail);
+        $view->setVar('defaultUsername', $defaultUsername);
+        $view->setVar('defaultDateOfBirth', $defaultDateOfBirth);
+        $view->setVar('defaultGender', $defaultGender);
         $view->setVar('email', $email);
-        $view->setVar('pseudo', $username);
+        $view->setVar('username', $username);
         $view->setVar('dateOfBirth', $dateOfBirth);
         $view->setVar('gender', $gender);
-        $view->setVar('defaultEmail', $verifEmail);
-        $view->setVar('defaultPseudo', $verifUsername);
-        $view->setVar('defaultDateOfBirth', $verifDateOfBirth);
-        $view->setVar('defaultGender', $verifGender);
+        $view->setVar('update', $update);
         $sameUsername = false;
         $sameEmail = false;
+        // appel des fonctions pour récupérer les emails et les pseudo de tous les utilisateur inscrits sur le site
         $verifSameEmail = $this->accountsService->getEmails($pdo);
         $verifSameUsername = $this->accountsService->getUsernames($pdo);
-        while($row = $verifSameEmail->fetch() && !$sameEmail) {
-            if($row == $email) {
+        // vérification que l'email envoyé n'existe pas déjà dans la base de données
+        while($row = $verifSameEmail->fetch()) {
+            if(strcmp($row->User_Email, $email) == 0) {
                 $sameEmail = true;
             }
         }
-        while($row = $verifSameUsername->fetch() && !$sameUsername) {
-            if($row == $email) {
+        // vérification que le pseudo envoyé n'existe pas déjà dans la base de données
+        while($row = $verifSameUsername->fetch()) {
+            if(strcmp($row->User_Name, $username) == 0) {
                 $sameUsername = true;
             }
         }
-        if(!empty($update) && !empty($email) && $email != $verifEmail && !$sameEmail) {
-            $this->accountsService->editMail($pdo, $email);
-            $view->setVar('mailChanged', true);              
+        echo var_dump($sameUsername);
+        echo var_dump($sameEmail);        
+        // si l'email n'est pas vide et qu'il n'existe pas alors on l'email est modifié
+        if(!empty($update) && !empty($email) && $email != $defaultEmail && $sameEmail == false) {
+            $this->accountsService->editMail($pdo, $email);              
             $view->setVar('message', "Vos informations ont bien été changées !");
-        } else {
+        } else if(!empty($update) && !empty($email) && $email != $defaultEmail && $sameEmail == true) {
             $view->setVar('message', "Email déjà existante !");
         }
-        if(!empty($update) && !empty($username) && $username != $verifUsername && !$sameUsername) {
+        // si le pseudo n'est pas vide et qu'il n'existe pas alors le pseudo est changé
+        if(!empty($update) && !empty($username) && $username != $defaultUsername && $sameUsername == false) {
             $this->accountsService->editUsername($pdo, $username);
-            $view->setVar('usernameChanged', true);
             $view->setVar('message', "Vos informations ont bien été changées !");
-        } else {
+        } else if(!empty($update) && !empty($username) && $username != $defaultUsername && $sameUsername == true) {
             $view->setVar('message', "Pseudonyme déjà existant !");
         }
-        if(!empty($dateOfBirth) && $dateOfBirth != $verifDateOfBirth) {
-            $this->accountsService->editDateOfBirth($pdo, $dateOfBirth);
-            $view->setVar('birthDateChanged', true);
+        // si la date de naissance n'est pas la même que celle stocké dans la base de données pour l'utilisateur courant alors elle est modifiée
+        if(!empty($dateOfBirth) && $dateOfBirth != $defaultDateOfBirth && $dateOfBirth < date("Y-m-d")) {
+            $defaultDateOfBirth = $this->accountsService->editDateOfBirth($pdo, $dateOfBirth);
+            $view->setVar('defaultDateOfBirth', $defaultDateOfBirth);
             $view->setVar('message', "Vos informations ont bien été changées !");
+        } else if($dateOfBirth != $defaultDateOfBirth && $dateOfBirth > date("Y-m-d")) {
+            $view->setVar('message', "Votre date de naissance ne peut pas être supérieur à la date d'aujourd'hui !");
         }
-        if(!empty($gender) && $gender != $verifGender) {
+        // si le genre n'est pas le même que celui stocké dans la base de donnée alors il est modifié
+        if(!empty($gender) && $gender != $defaultGender) {
             $this->accountsService->editGender($pdo, $gender);
-            $view->setVar('genderChanged', true);
             $view->setVar('message', "Vos informations ont bien été changées !");
         }
         return $view;
