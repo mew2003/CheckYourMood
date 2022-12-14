b0b45cbbc6d5fef9a7ec8c0a33b5dc5b2ef311f7
aVeRySsWood <axel.guilhot@gmail.com>
2022-12-14 10:32:57 +0100

    tableau des stats

--- codeCYM/views/Stats.php
+++ codeCYM/views/Stats.php
@@ -14,124 +14,20 @@
         spl_autoload_register();
     ?>
     <header-component></header-component>
-    <canvas id="myChart"></canvas>
-    <canvas id="myDonuts"></canvas>
-        <script>
-// ###################################################################################################
-// ##  Mon Graphe à Bar  #############################################################################
-// ###################################################################################################
-            var ctx = document.getElementById("myChart");
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: <?php 
-                    $i = 0;
-                        while ($row = $MaxHumeur->fetchColumn()) {
-                            if($i == 0) {
-                                echo "[";
-                            }
-                            echo "\"$row\",";
-                            if ($i == 3) {
-                                echo "]";
-                            }
-                            $i++;
-                        }
-                    ?>,
-                    datasets: [{
-                        label: 'Les 4 humeurs les plus présente chez vous',
-                        data: <?php 
-                            $i = 0;
-                            while ($row = $MaxValHum->fetchColumn()) {
-                                if($i == 0) {
-                                    echo "[";
-                                }
-                                echo "\"$row\",";
-                                if ($i == 3) {
-                                    echo "]";
-                                }
-                                $i++;
-                            }
-                        ?>,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }],
-                        yAxes: [{
-                            ticks: {
-                                fontSize: 40
-                            }
-                        }]
-                    }
-                }
-            });
-// ###################################################################################################
-// #####  Mon donuts   ###############################################################################
-// ###################################################################################################
-            var ctxDonuts = document.getElementById("myDonuts");
-            const data = {
-                labels:<?php 
-                    $i = 1;  
-                    $counter = $TotalOfHumeur->fetchColumn();
-                        while ($row = $AllHumeur->fetchColumn()) {
-                            if($i == 1) {
-                                echo "[";
-                            }
-                            echo "\"$row\",";
-                            if ($i == $counter) {
-                                echo "]";
-                            }
-                            $i++;
-                        }
-                    ?>,
-                datasets: [{
-                    label: 'Toute vos humeurs',
-                    data: <?php 
-                        $i = 1;
-                        while ($row = $AllHumeurData->fetchColumn()) {
-                            if($i == 1) {
-                                echo "[";
-                            }
-                            echo "\"$row\",";
-                            if ($i == $counter) {
-                                echo "]";
-                            }
-                            $i++;
-                        }
-                    ?>,
-                    backgroundColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(54, 162, 235)',
-                    'rgb(255, 205, 86)'
-                    ],
-                    hoverOffset: 4
-                }]
-            };
-            var myDonuts = new Chart(ctxDonuts, {
-                type: 'doughnut',
-                data: data,
-            });
-        </script>
+   
+    <table>
+        <tr class="title">
+            <td class="top-float-part"></td>
+            <td class="top-const-part"></td>
+        </tr>
+        <tr class="second-part">
+            <td class="mid-float-part"></td>
+            <td class="mid-const-part"></td>
+        </tr>
+        <tr class="third-part">
+            <td class="bot-float-part"></td>
+            <td class="bot-const-part"></td>
+        </tr>
+    </table>
     </body>
 </html>
