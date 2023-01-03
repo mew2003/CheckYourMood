4bbdc42519899193edb7c75dc0b5ae546f45ec52
aVeRySsWood <90342894+aVeRySsWood@users.noreply.github.com>
2023-01-03 18:00:58 +0100

    résolution pb

--- codeCYM/index.php
+++ codeCYM/index.php
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-
-spl_autoload_extensions(".php");
-spl_autoload_register();
-
-use yasmf\DataSource;
-use yasmf\Router;
-
-$dataSource = new DataSource(
-    $host = 'localhost',
-    $port = '3306', # to change with the port your mySql server listen to
-    $db = 'CYM', # to change with your db name
-    $user = 'root', # to change with your db user name
-    $pass = 'root', # to change with your db password
-    $charset = 'utf8mb4'
-);
-
-$router = new Router() ;
-$router->route($dataSource);
-=======
 <?php
 
 spl_autoload_extensions(".php");
@@ -38,4 +17,3 @@ $dataSource = new DataSource(
 
 $router = new Router() ;
 $router->route($dataSource);
->>>>>>> Stashed changes
